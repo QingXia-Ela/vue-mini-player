b0d33ff5c7877f31ab4904951b50f7e3071dbfc2
QingXia-Ela <Elahere@163.com>
2022-11-18 11:02:20 +0800

    feat: Change template to a module

--- src/main.ts
+++ src/main.ts
@@ -4,209 +4,212 @@ import './css/index.css'
 import './assets/fonts/iconfont/iconfont.css'
 import Adsorb from './directives/wrapperAdsorb'
 import execSecTime from './utils/execSecTime'
+import template from './constant/template'
 
-const id = 'CoreWrapper'
+(function () {
+  const id = 'CoreWrapper'
 
-const store: MusicPlayerCore = PetiteVue.reactive(new MusicPlayerCore({}))
-/** @ts-ignore */
-window._PlayerCore = store
+  const store: MusicPlayerCore = PetiteVue.reactive(new MusicPlayerCore({}))
+  /** @ts-ignore */
+  window._PlayerCore = store
 
-const _p = document.createElement('div')
-_p.setAttribute('id', '_player')
+  const _p = document.createElement('div')
 
-_p.innerHTML = ``
+  _p.setAttribute('id', 'CorePlayer')
+  _p.innerHTML += template
 
-let hiddenTimer: number
+  document.body.appendChild(_p)
 
-const app = PetiteVue.createApp({
-  store,
-  dragging: false,
-  onRight: true,
-  hidden: true,
-  hover: false,
-  currentTime: 0,
-  duration: 0,
-  showupPrecentage: 0,
-  showList: false,
-  sliding: false,
-  slidingTime: 0,
-  onMounted() {
-    const { e } = store
+  let hiddenTimer: number
 
-    e.addEventListener('timeupdate', () => {
-      if (!this.sliding) {
-        this.CurrentTime = e.currentTime
-        this._UpdateShowPrecentage(this.CurrentTime / this.duration)
+  const app = PetiteVue.createApp({
+    store,
+    dragging: false,
+    onRight: true,
+    hidden: true,
+    hover: false,
+    currentTime: 0,
+    duration: 0,
+    showupPrecentage: 0,
+    showList: false,
+    sliding: false,
+    slidingTime: 0,
+    onMounted() {
+      const { e } = store
+
+      e.addEventListener('timeupdate', () => {
+        if (!this.sliding) {
+          this.CurrentTime = e.currentTime
+          this._UpdateShowPrecentage(this.CurrentTime / this.duration)
+        }
+      })
+
+      e.addEventListener('loadedmetadata', () => {
+        if (!this.sliding)
+          this.duration = e.duration
+      })
+
+      this.store.e.addEventListener('ended', () => {
+        const { SongIdList, CurrentSongId, PlayMode } = this.store
+        const p = SongIdList.indexOf(CurrentSongId)
+        switch (PlayMode) {
+          case 1:
+            if (p !== SongIdList.length) store.NextSong()
+            break;
+          case 2:
+            store.NextSong()
+            break;
+          case 3:
+            store.Play()
+            break;
+          case 4:
+            let l = parseInt(SongIdList.length * Math.random() + '')
+            while (l === CurrentSongId) {
+              l = parseInt(SongIdList.length * Math.random() + '')
+            }
+            store.PlaySelectSong(SongIdList[l])
+            break;
+          default:
+            store.NextSong()
+            break;
+        }
+      })
+    },
+    get currentPrecentage() {
+      return (this.currentTime * 100).toFixed(2)
+    },
+    get currentSongInfo() {
+      const s = store.SongIdMap[store.CurrentSongId]
+      return s ? s : {
+        name: '_(:з」∠)_'
       }
-    })
-
-    e.addEventListener('loadedmetadata', () => {
-      if (!this.sliding)
-        this.duration = e.duration
-    })
-
-    this.store.e.addEventListener('ended', () => {
-      const { SongIdList, CurrentSongId, PlayMode } = this.store
-      const p = SongIdList.indexOf(CurrentSongId)
-      switch (PlayMode) {
-        case 1:
-          if (p !== SongIdList.length) store.NextSong()
-          break;
-        case 2:
-          store.NextSong()
-          break;
-        case 3:
-          store.Play()
-          break;
-        case 4:
-          let l = parseInt(SongIdList.length * Math.random() + '')
-          while (l === CurrentSongId) {
-            l = parseInt(SongIdList.length * Math.random() + '')
-          }
-          store.PlaySelectSong(SongIdList[l])
-          break;
-        default:
-          store.NextSong()
-          break;
+    },
+    get mediaDuration() {
+      return execSecTime(this.duration)
+    },
+    get mediaCurrentTime() {
+      return this.sliding ? execSecTime(this.slidingTime) : execSecTime(this.CurrentTime)
+    },
+    get songInfoList() {
+      const res = []
+      for (const i of store.SongIdList) {
+        res.push(store.SongIdMap[i])
       }
-    })
-  },
-  get currentPrecentage() {
-    return (this.currentTime * 100).toFixed(2)
-  },
-  get currentSongInfo() {
-    const s = store.SongIdMap[store.CurrentSongId]
-    return s ? s : {
-      name: '_(:з」∠)_'
-    }
-  },
-  get mediaDuration() {
-    return execSecTime(this.duration)
-  },
-  get mediaCurrentTime() {
-    return this.sliding ? execSecTime(this.slidingTime) : execSecTime(this.CurrentTime)
-  },
-  get songInfoList() {
-    const res = []
-    for (const i of store.SongIdList) {
-      res.push(store.SongIdMap[i])
-    }
-    return res
-  },
-  clearHiddenTimer() {
-    clearTimeout(hiddenTimer)
-  },
-  startTimer() {
-    if (hiddenTimer)
+      return res
+    },
+    clearHiddenTimer() {
+      clearTimeout(hiddenTimer)
+    },
+    startTimer() {
+      if (hiddenTimer)
+        this.clearHiddenTimer()
+      hiddenTimer = setTimeout(() => {
+        this.hidden = true
+        this.hover = false
+        this.showList = false
+        this.clearHiddenTimer()
+      }, 8000)
+    },
+    /** wrapper */
+    wrapperMouseDown() {
+      this.dragging = true
+      this.hidden = false
+    },
+    wrapperMouseUp() {
+      this.dragging = false
+      const e = document.getElementById(id)
+      if (e) {
+        if (e.offsetLeft > window.innerWidth / 2) this.onRight = true
+        else this.onRight = false
+      }
+    },
+    wrapperMouseEnter() {
+      this.hover = true
       this.clearHiddenTimer()
-    hiddenTimer = setTimeout(() => {
-      this.hidden = true
+    },
+    wrapperMouseLeave() {
       this.hover = false
-      this.showList = false
-      this.clearHiddenTimer()
-    }, 8000)
-  },
-  /** wrapper */
-  wrapperMouseDown() {
-    this.dragging = true
-    this.hidden = false
-  },
-  wrapperMouseUp() {
-    this.dragging = false
-    const e = document.getElementById(id)
-    if (e) {
-      if (e.offsetLeft > window.innerWidth / 2) this.onRight = true
-      else this.onRight = false
-    }
-  },
-  wrapperMouseEnter() {
-    this.hover = true
-    this.clearHiddenTimer()
-  },
-  wrapperMouseLeave() {
-    this.hover = false
-    this.startTimer()
-  },
-  clickShow(e: MouseEvent) {
-    clearTimeout(hiddenTimer)
-    this.hidden = !this.hidden
-    if (this.hidden)
-      this.showList = false
-    e.preventDefault();
-    this.startTimer()
-  },
-  _UpdateShowPrecentage(p: any) {
-    if (p > 1) p = 1
-    else if (p < 0) p = 0
-    this.showupPrecentage = (p * 100).toFixed(2)
-    this.slidingTime = this.duration * p
-  },
-  jumpTime(e: MouseEvent) {
-    if (isNaN(this.store.e.duration)) return
-    const slider = e.currentTarget
-    // @ts-expect-error
-    let p = e.offsetX / slider.clientWidth
-    this._UpdateShowPrecentage(p)
-    this.sliding = true
-    // exec p 2 slider length
-    // @ts-expect-error
-    p *= slider.clientWidth
-
-    const slide = (e: MouseEvent) => {
-      const s = document.getElementById(id)
+      this.startTimer()
+    },
+    clickShow(e: MouseEvent) {
+      clearTimeout(hiddenTimer)
+      this.hidden = !this.hidden
+      if (this.hidden)
+        this.showList = false
+      e.preventDefault();
+      this.startTimer()
+    },
+    _UpdateShowPrecentage(p: any) {
+      if (p > 1) p = 1
+      else if (p < 0) p = 0
+      this.showupPrecentage = (p * 100).toFixed(2)
+      this.slidingTime = this.duration * p
+    },
+    jumpTime(e: MouseEvent) {
+      if (isNaN(this.store.e.duration)) return
+      const slider = e.currentTarget
       // @ts-expect-error
-      p = (e.clientX - s.offsetLeft - slider.clientWidth)
-      p += this.onRight ? 25 : 85
+      let p = e.offsetX / slider.clientWidth
+      this._UpdateShowPrecentage(p)
+      this.sliding = true
+      // exec p 2 slider length
       // @ts-expect-error
-      this._UpdateShowPrecentage(p / slider.clientWidth)
-    }
-    const cs = () => {
-      // @ts-expect-error
-      this.store.e.currentTime = (p / slider.clientWidth) * this.duration
-      this.sliding = false
-      window.removeEventListener('mousemove', slide)
-      window.removeEventListener('mouseup', cs)
-    }
-    window.addEventListener('mousemove', slide)
-    window.addEventListener('mouseup', cs)
-  },
-  switchListShow() {
-    this.showList = !this.showList
-  },
-  async CorePlay() {
-    try {
-      await store.Play()
-    } catch (err) {
-      console.log(err);
-    }
-  },
-  CorePause() {
-    store.Pause()
-  },
-  CorePlaySelectSong(id: number) {
-    store.PlaySelectSong(id)
-  }
-})
-app.directive('draggable', Draggable)
-/** wrapper directive */
-app.directive('wrapper-adsorb', Adsorb)
-app.mount()
+      p *= slider.clientWidth
 
-setTimeout(() => {
-  store.AppendSong({
-    name: 'Infected',
-    id: 0,
-    src: 'https://shiinafan.top/static/sample.mp3',
-    img: 'https://shiinafan.top/favicon.ico'
+      const slide = (e: MouseEvent) => {
+        const s = document.getElementById(id)
+        // @ts-expect-error
+        p = (e.clientX - s.offsetLeft - slider.clientWidth)
+        p += this.onRight ? 25 : 85
+        // @ts-expect-error
+        this._UpdateShowPrecentage(p / slider.clientWidth)
+      }
+      const cs = () => {
+        // @ts-expect-error
+        this.store.e.currentTime = (p / slider.clientWidth) * this.duration
+        this.sliding = false
+        window.removeEventListener('mousemove', slide)
+        window.removeEventListener('mouseup', cs)
+      }
+      window.addEventListener('mousemove', slide)
+      window.addEventListener('mouseup', cs)
+    },
+    switchListShow() {
+      this.showList = !this.showList
+    },
+    async CorePlay() {
+      try {
+        await store.Play()
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    CorePause() {
+      store.Pause()
+    },
+    CorePlaySelectSong(id: number) {
+      store.PlaySelectSong(id)
+    }
   })
-  store.AppendSong({
-    name: 'olk', id: 1, src: 'https://shiinafan.top/static/sample2.mp3',
-    img: 'https://shiinafan.top/favicon.ico'
-  })
-  store.AppendSong({
-    name: 'Untitled World', id: 2, src: 'https://shiinafan.top/static/sample3.mp3',
-    img: 'https://shiinafan.top/favicon.ico'
-  })
-});
+  app.directive('draggable', Draggable)
+  /** wrapper directive */
+  app.directive('wrapper-adsorb', Adsorb)
+  app.mount("#CorePlayer")
 
-// document.body.appendChild(_p)
+  setTimeout(() => {
+    store.AppendSong({
+      name: 'Infected',
+      id: 0,
+      src: 'https://shiinafan.top/static/sample.mp3',
+      img: 'https://shiinafan.top/favicon.ico'
+    })
+    store.AppendSong({
+      name: 'olk', id: 1, src: 'https://shiinafan.top/static/sample2.mp3',
+      img: 'https://shiinafan.top/favicon.ico'
+    })
+    store.AppendSong({
+      name: 'Untitled World', id: 2, src: 'https://shiinafan.top/static/sample3.mp3',
+      img: 'https://shiinafan.top/favicon.ico'
+    })
+  });
+})()
